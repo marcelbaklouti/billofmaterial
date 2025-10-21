ddb5ea81d2d8b6d042eeab61c3cb9e90dc85f63b
Marcel Baklouti <marcel@baklouti.de>
2025-10-21 18:07:04 +0200

    chore: bump version to v0.0.4

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -12,7 +12,7 @@ const program = new Command();
 program
   .name('billofmaterial')
   .description('Generate comprehensive Software Bill of Materials (SBOM) for your projects')
-  .version('0.0.3');
+  .version('0.0.4');
 
 program
   .command('generate')
